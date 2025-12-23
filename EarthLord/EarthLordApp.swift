b2e918e38798dbf31ec9264072bab4ca94c4b44b
yuleira <rachelyulei@gmail.com>
2025-12-23 13:18:13 +0100

    Initial Commit

---
+++ EarthLord/EarthLordApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthLordApp.swift
+//  EarthLord
+//
+//  Created by Yu Lei on 23/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct EarthLordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
